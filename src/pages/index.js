ee5e0080e45f5915b1bb1dcbef8bce8e951a2b9b
Mici Srsr <micipsa.sersour@epitech.eu>
2022-01-28 19:49:13 +0100

    add gatsby image plugin

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,15 @@
 import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/Layout"
+import { StaticImage } from "gatsby-plugin-image"
 
 export default function Home() {
   return (
     <Layout>
-        <h1>home page</h1>
+      <h1>home page</h1>
+
+      <StaticImage src="https://placekitten.com/800/600" alt="A kitten" />
+
     </Layout>
   )
 }
