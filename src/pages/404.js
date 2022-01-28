77efe94ae15ca1aaaf21fad1e37c5518eabdeead
Mici Srsr <micipsa.sersour@epitech.eu>
2022-01-28 15:45:24 +0100

    add contact page

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,6 +4,7 @@ import Layout from '../components/Layout'
 const Error = () => {
     return (
         <Layout>
+            {/* <SEO title="Error" /> */}
             <main className="error-page">
                 <section>
                     <h1>404</h1>
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,9 +4,48 @@ import Layout from '../components/Layout'
 const Contact = () => {
     return (
         <Layout>
-            <h1>contact page</h1>
+            {/* <SEO title="Contact" /> */}
+            <main className="page">
+                <section className="contact-page">
+                    <article className="contact-info">
+                        <h3>Want To Get In Touch?</h3>
+                        <p>
+                            Four dollar toast biodiesel plaid salvia actually pickled banjo
+                            bespoke mlkshk intelligentsia edison bulb synth.
+              </p>
+                        <p>Cardigan prism bicycle rights put a bird on it deep v.</p>
+                        <p>
+                            Hashtag swag health goth air plant, raclette listicle fingerstache
+                            cold-pressed fanny pack bicycle rights cardigan poke.
+              </p>
+                    </article>
+                    <article>
+                        <form className="form contact-form">
+                            <div className="form-row">
+                                <label htmlFor="name">your name</label>
+                                <input type="text" name="name" id="name" />
+                            </div>
+                            <div className="form-row">
+                                <label htmlFor="email">your email</label>
+                                <input type="text" name="email" id="email" />
+                            </div>
+                            <div className="form-row">
+                                <label htmlFor="message">message</label>
+                                <textarea name="message" id="message"></textarea>
+                            </div>
+                            <button type="submit" className="btn block">
+                                submit
+                </button>
+                        </form>
+                    </article>
+                </section>
+                <section className="featured-recipes">
+                    <h5>Look at this Awesomesouce!</h5>
+                    {/* <RecipesList recipes={recipes} /> */}
+                </section>
+            </main>
         </Layout>
     )
 }
 
-export default Contact 
+export default Contact
