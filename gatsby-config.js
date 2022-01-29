4e8db8a2212e071b3ae5ca5f73cb4c81dd504c6d
Mici Srsr <micipsa.sersour@epitech.eu>
2022-01-29 19:32:14 +0100

    add gatsby source filesystem plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,11 +10,11 @@ module.exports = {
     title: "Simply Recipes",
     description: "Nice and clean recipes site",
     author: "MiciWeb",
-    person:{name:"mici",age:19},
-    simpleData:["item 1","item 2"],
-    complexData:[
-      {name:"marie",age:26},
-      {name:"vin's",age:17},
+    person: { name: "mici", age: 19 },
+    simpleData: ["item 1", "item 2"],
+    complexData: [
+      { name: "marie", age: 26 },
+      { name: "vin's", age: 17 },
     ]
   },
   plugins: [
@@ -22,5 +22,12 @@ module.exports = {
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/assets/images`,
+      },
+    },
   ],
 }
--- src/examples/fetchData.js
+++ src/examples/fetchData.js
@@ -1,35 +1,34 @@
-import React from "react"
+import React from 'react'
 import { useStaticQuery, graphql } from "gatsby"
 
-const ComponentName = () => {
-  const data = useStaticQuery(graphql`
-    {
-      site {
-        siteMetadata {
-          complexData {
-            age
-            name
-          }
-          person {
-            age
-            name
-          }
-          author
-          simpleData
-          title
-        }
+const getData = graphql`
+query {
+  site {
+    info:siteMetadata {
+      complexData {
+        age
+        name
       }
+      person {
+        age
+        name
+      }
+      author
+      simpleData
+      title
     }
-  `)
-  return <div>
-    <h2>{data.site.siteMetadata.person.name}</h2>
+  }
+}
+`
+
+const FetchData = () => {
+  const { site: { info: { person: {name} } } } = useStaticQuery(getData)
+  return (
     <div>
-      {data.site.siteMetadata.complexData.map((item, index) => {
-        return <p key={index}>{item.name}:{item.age}</p>
-      })}
+      {/* <h2> Name : {data.site.siteMetadata.person.name}</h2> */}
+        {name}
     </div>
-  </div>
+  )
 }
 
-export default ComponentName
-
+export default FetchData
