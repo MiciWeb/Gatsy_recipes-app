46c5b716e452bba3ba45bde27356c15cf564f8f7
Mici Srsr <micipsa.sersour@epitech.eu>
2022-01-29 10:56:29 +0100

    add index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,25 @@ import { StaticImage } from "gatsby-plugin-image"
 export default function Home() {
   return (
     <Layout>
-      <h1>home page</h1>
+      {/* <SEO title="Home " /> */}
+      <main className="page">
+        <header className="hero">
+          <StaticImage
+            src="../assets/images/main.jpeg"
+            alt="eggs"
+            className="hero-img"
+            placeholder="tracedSVG"
+            layout="fullWidth"
+          ></StaticImage>
+          <div className="hero-container">
+            <div className="hero-text">
+              <h1>simply recipes</h1>
+              <h4>no fluff, just recipes</h4>
+            </div>
+          </div>
+        </header>
+        {/* <AllRecipes /> */}
+      </main>
     </Layout>
   )
 }
