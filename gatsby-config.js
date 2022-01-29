03b262bba178b90313b0ece22a247f4f25857bcf
Mici Srsr <micipsa.sersour@epitech.eu>
2022-01-29 11:30:51 +0100

    add first graphql test

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,17 @@
 
 module.exports = {
   /* Your site config here */
+  siteMetadata: {
+    title: "Simply Recipes",
+    description: "Nice and clean recipes site",
+    author: "MiciWeb",
+    person:{name:"mici",age:19},
+    simpleData:["item 1","item 2"],
+    complexData:[
+      {name:"marie",age:26},
+      {name:"vin's",age:17},
+    ]
+  },
   plugins: [
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-image`,
